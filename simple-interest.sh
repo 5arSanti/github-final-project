6c48d3dd88036e19ee9e4eea12d81aa6e9915e01
5arSanti <129137917+5arSanti@users.noreply.github.com>
2023-04-11 10:07:22 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # 5ar_Santi
+   # 5arSanti
    # Input:
    # p, principal amount
    # t, time period in years
